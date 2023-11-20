c60e240ff9c73b611a49206e64ad291c4d0a534b
codewizard870 <alenzer0902@gmail.com>
2023-11-20 12:08:18 -0500

    no infinite on leaderboard cards

--- src/pages/leaderboard/cards/index.tsx
+++ src/pages/leaderboard/cards/index.tsx
@@ -5,26 +5,16 @@ import "./index.css";
 
 const Cards = () => {
   const settings = {
-    infinit: false,
+    infinite: false,
     speed: 500,
-    slidesToShow: 3,
+    slidesToShow: 2,
     slidesToScroll: 1,
     responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          infinite: true,
-          dots: true,
-        },
-      },
       {
         breakpoint: 600,
         settings: {
           slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
+          slidesToScroll: 1,
         },
       },
       {
