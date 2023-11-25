b817cc2e07c716a44109efa010142b4092eacee3
codewizard870 <alenzer0902@gmail.com>
2023-11-24 20:13:41 -0500

    border on defi activity

--- src/pages/credPoints/defiActivity/activityList/activityItem.tsx
+++ src/pages/credPoints/defiActivity/activityList/activityItem.tsx
@@ -33,7 +33,7 @@ const ActivityItem: React.FC<Props> = ({ data }) => {
           <img src="/credpoints/cred.svg" className="w-[22px] h-[22px]" alt="cred" />
         </div>
       </div>
-      <div className="h-px border border-gray-light-1 md:mx-8" />
+      <div className="h-px border border-gray-light-1" />
     </a>
   );
 };
