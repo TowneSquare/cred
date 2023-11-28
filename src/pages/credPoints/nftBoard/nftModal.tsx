7ca22110b9ec3a11030dce4585fd6803b6414057
codewizard870 <alenzer0902@gmail.com>
2023-11-28 05:02:01 -0500

    live

--- src/pages/credPoints/nftBoard/nftModal.tsx
+++ src/pages/credPoints/nftBoard/nftModal.tsx
@@ -21,14 +21,15 @@ const NftModal = () => {
   }, [isOpen]);
   return (
     <div
-      className={`fixed ${isOpen ? "block" : "hidden"} inset-0 z-50 flex bg-gray-dark-2`}
-      onClick={(e) => dispatch(toggleNftList(false))}
+      className={`fixed ${
+        isOpen ? "block" : "hidden"
+      } inset-0 z-50 flex bg-gray-dark-2`}
     >
       <div
         ref={ref}
         className={`relative container mt-[100vh] opacity-0 mx-px mb-px flex justify-center border border-gray-light-1 rounded-md`}
         style={{
-          transition: "margin-top 0.5s, opacity 0.5s"
+          transition: "margin-top 0.5s, opacity 0.5s",
         }}
       >
         <NftList />
