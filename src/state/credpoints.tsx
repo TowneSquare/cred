bf1c1d76f07a87fca3491dfabd87676dc85b008c
codewizard870 <alenzer0902@gmail.com>
2023-11-28 05:25:11 -0500

    live data

--- src/state/credpoints.tsx
+++ src/state/credpoints.tsx
@@ -1,65 +1,63 @@
-import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
-import { DefiActivityType } from '../type/defiActivityType';
-import { NftType } from '../type/nftType';
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import { DefiActivityType } from "../type/defiActivityType";
+import { NftType } from "../type/nftType";
 import Moralis from "moralis";
 
 interface credpointsStates {
-   isLive: boolean;
-   totalPoint: number;
-   defiPoint: number;
-   nftPoint: number;
-   defiActivities: DefiActivityType[];
-   nfts: NftType[];
-   rewardNFTPointPerDay: number;
-};
+  isLive: boolean;
+  totalPoint: number;
+  defiPoint: number;
+  nftPoint: number;
+  defiActivities: DefiActivityType[];
+  nfts: NftType[];
+  rewardNFTPointPerDay: number;
+}
 
 const initialState: credpointsStates = {
-   isLive: false,
-   totalPoint: 0,
-   defiPoint: 0,
-   nftPoint: 0,
-   defiActivities: [],
-   nfts: [],
-   rewardNFTPointPerDay: 0,
+  isLive: false,
+  totalPoint: 0,
+  defiPoint: 0,
+  nftPoint: 0,
+  defiActivities: [],
+  nfts: [],
+  rewardNFTPointPerDay: 0,
 };
 
 export const fetchCredpoints = createAsyncThunk(
-   'credpoints/fetch',
-   async (wallet: string, thunkAPI) => {
-      const url = `https://backend.townesquare.xyz/activity/point/${wallet}`;
-      try {
-         const res = await fetch(url);
-         const result = await res.json();
-         return result;
-      } catch (error: any) {
-         return thunkAPI.rejectWithValue(error.response.data);
-      }
-   }
+  "credpoints/fetch",
+  async (wallet: string, thunkAPI) => {
+    const url = `https://backend.townesquare.xyz/activity/point/${wallet}`;
+    console.log(url);
+    try {
+      const res = await fetch(url);
+      const result = await res.json();
+      return result;
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
 );
 
-
 export const credpointsSlice = createSlice({
-   name: 'credPoints',
-   initialState,
-   reducers: {
-      
-   },
-   extraReducers: (builder) => {
-      builder.addCase(fetchCredpoints.fulfilled, (state, action) => {
-         console.log(action.payload)
-         state.isLive = true;
-         state.totalPoint = action.payload.totalPoint;
-         state.defiPoint = action.payload.defiPoint;
-         state.nftPoint = action.payload.nftPoint;
-         state.defiActivities = action.payload.defiActivityList;
-         state.nfts = action.payload.nftActivityList;
-         state.rewardNFTPointPerDay = action.payload.rewardNFTPointPerDay ?? state.nfts.length * 50;
-      });
-   },
+  name: "credPoints",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchCredpoints.fulfilled, (state, action) => {
+      console.log(action.payload);
+      if (!action.payload.statusCode) {
+        state.isLive = true;
+        state.totalPoint = action.payload.totalPoint;
+        state.defiPoint = action.payload.defiPoint;
+        state.nftPoint = action.payload.nftPoint;
+        state.defiActivities = action.payload.defiActivityList;
+        state.nfts = action.payload.nftActivityList;
+        state.rewardNFTPointPerDay =
+          action.payload.rewardNFTPointPerDay ?? state.nfts.length * 50;
+      }
+    });
+  },
 });
 
-export const {
-} = credpointsSlice.actions;
+export const {} = credpointsSlice.actions;
 export default credpointsSlice.reducer;
-
-
--- src/state/leaderboard.tsx
+++ src/state/leaderboard.tsx
@@ -1,52 +1,53 @@
-import { createSlice, createAsyncThunk, PayloadAction } from '@reduxjs/toolkit';
-import { RankingType } from '../type/rankingType';
+import { createSlice, createAsyncThunk, PayloadAction } from "@reduxjs/toolkit";
+import { RankingType } from "../type/rankingType";
 
 interface leaderboardStates {
-   myRank: number;
-   myMorePoint: number;
-   lowerPercentage: number;
-   topRankings: RankingType[];
-};
-
-const initialState: leaderboardStates = {
-   myRank: 0,
-   myMorePoint: 0,
-   lowerPercentage: 0,
-   topRankings: []
+  myRank: number;
+  myMorePoint: number;
+  lowerPercentage: number;
+  topRankings: RankingType[];
 }
 
+const initialState: leaderboardStates = {
+  myRank: 0,
+  myMorePoint: 0,
+  lowerPercentage: 0,
+  topRankings: [],
+};
+
 export const fetchRankings = createAsyncThunk(
-   'leaderboard/fetch',
-   async (wallet: string, thunkAPI) => {
-      const url = `https://backend.townesquare.xyz/activity/leaderboard/${wallet}`;
-      try {
-         const res = await fetch(url);
-         return await res.json();
-      } catch (error: any) {
-         return thunkAPI.rejectWithValue(error.response.data);
-      }
-   }
+  "leaderboard/fetch",
+  async (wallet: string, thunkAPI) => {
+    const url = `https://backend.townesquare.xyz/activity/leaderboard/${wallet}`;
+    console.log(url);
+    try {
+      const res = await fetch(url);
+      const result = await res.json();
+      return result;
+    } catch (error: any) {
+      return thunkAPI.rejectWithValue(error.response.data);
+    }
+  }
 );
 
-
 export const leaderboardSlice = createSlice({
-   name: 'leaderboard',
-   initialState,
-   reducers: {
-      
-   },
-   extraReducers: (builder) => {
-      builder.addCase(fetchRankings.fulfilled, (state, action) => {
-         state.myRank = action.payload.rank;
-         state.topRankings = action.payload.topRankings;
-         state.myMorePoint = action.payload.morePoint;
-         state.lowerPercentage = action.payload.lowerPercentage;
-      });
-   },
+  name: "leaderboard",
+  initialState,
+  reducers: {},
+  extraReducers: (builder) => {
+    builder.addCase(fetchRankings.fulfilled, (state, action) => {
+      console.log(action.payload);
+      if (
+        !action.payload.statusCode
+      ) {
+        state.myRank = action.payload.rank;
+        state.topRankings = action.payload.topRankings;
+        state.myMorePoint = action.payload.morePoint;
+        state.lowerPercentage = action.payload.lowerPercentage;
+      }
+    });
+  },
 });
 
-export const {
-} = leaderboardSlice.actions;
+export const {} = leaderboardSlice.actions;
 export default leaderboardSlice.reducer;
-
-
