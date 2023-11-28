46f725e5d4767675f15d4da02dec34268f8c0ae9
codewizard870 <alenzer0902@gmail.com>
2023-11-28 00:40:58 -0500

    update paragraph on tos

--- src/pages/privacyPolicy/index.tsx
+++ src/pages/privacyPolicy/index.tsx
@@ -1,4 +1,5 @@
 import Header from "../../components/header";
+import { Content, Space, Title } from "../../components/paragraph";
 import XLink from "../../components/xlink";
 
 const PrivacyPolicy = () => {
@@ -7,104 +8,92 @@ const PrivacyPolicy = () => {
       <Header />
       <div className="w-full flex justify-center">
         <div className="w-full md:w-[700px] px-4 md:px-0 flex flex-col mt-44 mb-10">
-          <h1 className="text-3xl font-bold">Privacy Policy</h1>
-          <p className="text-sm mt-8 font-[inter]">
-            <span className="text-xl">1. Introduction</span>
-            <br />
-            <br />
+          <h1 className="text-[28px] md:text-[32px] font-bold">
+            Privacy Policy
+          </h1>
+          <Title>1. Introduction</Title>
+          <Content>
             Welcome to CRED, a web platform developed by eTown PTE. LTD. This
             Privacy Policy outlines how we collect, use, disclose, and protect
             your information when you use our website and services. By accessing
             or using CRED, you agree to the terms of this Privacy Policy.
-            <br />
-            <br />
-            <span className="text-xl">2. Information We Collect</span>
-            <br />
-            <br />
+          </Content>
+          <Title>2. Information We Collect</Title>
+          <Content>
             2.1. Wallet Information: When you sign in to CRED, you use your
             wallet to access the platform. We collect and store information
             associated with your wallet, including wallet address and related
             metadata.
-            <br />
-            <br />
+            <Space />
             2.2. CRED Points Calculation Data: CRED points are calculated based
             on your activity on the blockchain. We collect and process
             transaction and history data from blockchains to determine your CRED
             points. This information may include transaction amounts,
             timestamps, and blockchain addresses.
-            <br />
-            <br />
+            <Space />
             2.3. Open-Source Data: All data collected is sourced from
             open-source environments on the blockchain. We do not access private
             or sensitive information outside of the publicly available
             blockchain data.
-            <br />
-            <br />
-            <span className="text-xl">3. Use of Information</span>
-            <br />
-            <br />
+          </Content>
+          <Title>3. Use of Information</Title>
+          <Content>
             3.1. CRED Points Calculation: We use the collected data to calculate
             and display your CRED points on the platform, providing you with
             insights into your blockchain activity.
-            <br />
-            <br />
+          </Content>
+          <Space />
+          <Content>
             3.2. Service Improvement: The information collected is used to
             improve and enhance the functionality of CRED, ensuring a seamless
             and personalized user experience.
-            <br />
-            <br />
+          </Content>
+          <Space />
+          <Content>
             3.3. Security: We employ industry-standard security measures to
             protect your information and ensure the integrity of our platform.
-            <br />
-            <br />
-            <span className="text-xl">4. Information Sharing</span>
-            <br />
-            <br />
+          </Content>
+          <Title>4. Information Sharing</Title>
+          <Content>
             4.1. Third-Party Service Providers: We may engage third-party
             service providers to assist with data processing, storage, or
             analysis. These providers are bound by confidentiality agreements
             and only access information as necessary to perform their services.
-            <br />
-            <br />
+          </Content>
+          <Space />
+          <Content>
             4.2. Legal Compliance: We may disclose information when required by
             law or in response to a valid legal request.
-            <br />
-            <br />
-            <span className="text-xl">5. Data Retention</span>
-            <br />
-            <br />
+          </Content>
+          <Title>5. Data Retention</Title>
+          <Content>
             We retain your information for as long as necessary to provide our
             services and fulfill the purposes outlined in this Privacy Policy.
             If you wish to delete your account and associated data, please
             contact us at <XLink>@townesquarexyz</XLink> X account.
-            <br />
-            <br />
-            <span className="text-xl">6. Your Choices</span>
-            <br />
-            <br />
+          </Content>
+          <Title>6. Your Choices</Title>
+          <Content>
             You have the right to delete your personal information. If you have
             questions or concerns about your privacy, please contact us at
             <XLink>@townesquarexyz</XLink> X account.
-            <br />
-            <br />
-            <span className="text-xl">7. Changes to the Privacy Policy</span>
-            <br />
-            <br />
+          </Content>
+          <Title>7. Changes to the Privacy Policy</Title>
+          <Content>
             We may update this Privacy Policy to reflect changes in our
             practices. We will notify you of any material changes by posting the
             updated policy on the CRED website.
-            <br />
-            <br />
-            <span className="text-xl">8. Contact Information</span>
-            <br />
-            <br />
+          </Content>
+          <Title>8. Contact Information</Title>
+          <Content>
             If you have questions or concerns about this Privacy Policy, please
             contact us at <XLink>@townesquarexyz</XLink> X account.
-            <br />
-            <br />
+          </Content>
+          <Space />
+          <Content>
             By using CRED, you acknowledge that you have read and agree to this
             Privacy Policy.
-          </p>
+          </Content>
         </div>
       </div>
     </div>
--- src/pages/tos/index.tsx
+++ src/pages/tos/index.tsx
@@ -1,4 +1,5 @@
 import Header from "../../components/header";
+import { Content, Space, Title } from "../../components/paragraph";
 import XLink from "../../components/xlink";
 
 const TermsOfService = () => {
@@ -8,96 +9,78 @@ const TermsOfService = () => {
       <div className="w-full flex justify-center">
         <div className="w-full md:w-[700px] px-4 md:px-0 flex flex-col mt-44 mb-10">
           <h1 className="text-3xl font-bold">Terms of Service</h1>
-          <p className="text-sm mt-8 font-[inter]">
-            <span className="text-xl">1. Acceptance of Terms</span>
-            <br />
-            <br />
+          <Title>1. Acceptance of Terms</Title>
+          <Content>
             1.1. By accessing or using the CRED web platform ("CRED"), you agree
             to comply with and be bound by these Terms of Service ("TOS"). If
             you do not agree to these terms, please refrain from using CRED.
-            <br />
-            <br />
-            <span className="text-xl">2. Use of CRED</span>
-            <br />
-            <br />
+          </Content>
+          <Title>2. Use of CRED</Title>
+          <Content>
             2.1. CRED Points: CRED calculates and displays points based on your
             blockchain activity. The accuracy of these points is subject to the
             information provided by the blockchain, and we are not responsible
             for discrepancies or inaccuracies.
-            <br />
-            <br />
+          </Content>
+          <Space />
+          <Content>
             2.2. Account Security: You are responsible for maintaining the
             confidentiality of your wallet information used to access CRED.
             Notify us immediately of any unauthorized access or use of your
             account.
-            <br />
-            <br />
-            <span className="text-xl">3. User Responsibilities</span>
-            <br />
-            <br />
+          </Content>
+          <Title>3. User Responsibilities</Title>
+          <Content>
             3.1. Compliance: Users must comply with all applicable laws and
             regulations while using CRED.
-            <br />
-            <br />
+          </Content>
+          <Space />
+          <Content>
             3.2. Prohibited Activities: Users may not engage in any activities
             that violate the law, infringe on the rights of others, or disrupt
             the functionality of CRED.
-            <br />
-            <br />
-            <span className="text-xl">4. Intellectual Property</span>
-            <br />
-            <br />
+          </Content>
+          <Title>4. Intellectual Property</Title>
+          <Content>
             4.1. Trademarks and Copyrights: CRED and its logo are trademarks of
             eTown PTE. LTD.
-            <br />
-            <br />
-            <span className="text-xl">5. Disclaimers</span>
-            <br />
-            <br />
+          </Content>
+          <Title>5. Disclaimers</Title>
+          <Content>
             5.1. Use at Your Own Risk: Users acknowledge that the use of CRED is
             at their own risk. We provide no warranties regarding the accuracy,
             reliability, or availability of CRED.
-            <br />
-            <br />
-            <span className="text-xl">6. Limitation of Liability</span>
-            <br />
-            <br />
+          </Content>
+          <Title>6. Limitation of Liability</Title>
+          <Content>
             6.1. To the extent permitted by law, eTown PTE. LTD. shall not be
             liable for any direct, indirect, incidental, special, or
             consequential damages arising out of the use or inability to use
             CRED.
-            <br />
-            <br />
-            <span className="text-xl">7. Termination</span>
-            <br />
-            <br />
+          </Content>
+          <Title>7. Termination</Title>
+          <Content>
             7.1. eTown PTE. LTD. reserves the right to terminate or suspend
             access to CRED at its discretion, without prior notice, for any
             reason.
-            <br />
-            <br />
-            <span className="text-xl">8. Changes to Terms</span>
-            <br />
-            <br />
+          </Content>
+          <Title>8. Changes to Terms</Title>
+          <Content>
             8.1. We reserve the right to update these Terms of Service at any
             time. Users will be notified of material changes, and continued use
             of CRED constitutes acceptance of the updated terms.
-            <br />
-            <br />
-            <span className="text-xl">9. Governing Law</span>
-            <br />
-            <br />
+          </Content>
+          <Title>9. Governing Law</Title>
+          <Content>
             9.1. These Terms of Service are governed by and construed in
             accordance with the laws of Singapore, without regard to its
             conflict of law principles.
-            <br />
-            <br />
-            <span className="text-xl">10. Contact Information</span>
-            <br />
-            <br />
+          </Content>
+          <Title>10. Contact Information</Title>
+          <Content>
             10.1. For questions or concerns regarding these Terms of Service,
-            please contact us at <XLink>@townesquarexyz</XLink> X  account.
-          </p>
+            please contact us at <XLink>@townesquarexyz</XLink> X account.
+          </Content>
         </div>
       </div>
     </div>
