7e77fa7c69dc7e042eab787748eda0cd5ab64e6e
codewizard870 <alenzer0902@gmail.com>
2023-12-04 05:19:50 -0500

    update static twitter

--- src/components/share.tsx
+++ src/components/share.tsx
@@ -12,7 +12,7 @@ const Share = () => {
   return (
     <>
       <TwitterShareButton
-        url={`https://cred-beta.vercel.app/twitter`}
+        url={`https://cred-beta.vercel.app`}
         title={shareText}
       >
         <div className="min-w-[50px] px-6 py-3 md:p-4 flex justify-center gap-1 items-center border border-gray-light-1 hover:border-white rounded-full cursor-pointer">
