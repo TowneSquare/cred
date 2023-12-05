adf4ebaebb6bba6c1a1ebc25a4800548d9d43412
codewizard870 <alenzer0902@gmail.com>
2023-12-05 16:19:19 -0500

    live data

--- src/App.tsx
+++ src/App.tsx
@@ -23,20 +23,20 @@ function App() {
   const { connected, account } = useWallet();
 
   useEffect(() => {
-    // if (account) {
-    //   dispatch(fetchCredpoints(account.address));
-    //   dispatch(fetchRankings(account.address));
-    // }
-    dispatch(
-      fetchRankings(
-        "0xb52363ed75f496448b691d33125bd1a866cf35a0132626074f59d4e07bb80234"
-      )
-    );
-    dispatch(
-      fetchCredpoints(
-        "0x3bc474f3c3c37c9cdb6643c04e5004e5e03b17b1a4200ef807cd990f65b0e194"
-      )
-    );
+    if (account) {
+      dispatch(fetchCredpoints(account.address));
+      dispatch(fetchRankings(account.address));
+    }
+    // dispatch(
+    //   fetchRankings(
+    //     "0xb52363ed75f496448b691d33125bd1a866cf35a0132626074f59d4e07bb80234"
+    //   )
+    // );
+    // dispatch(
+    //   fetchCredpoints(
+    //     "0x3bc474f3c3c37c9cdb6643c04e5004e5e03b17b1a4200ef807cd990f65b0e194"
+    //   )
+    // );
   }, [connected, account]);
 
   return (
