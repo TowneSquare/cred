5e8bae05305f7d5a3fc13cf371e632d7cba46a4d
codewizard870 <alenzer0902@gmail.com>
2023-12-09 22:28:57 +0800

    mobile style

--- src/components/inviteCode.tsx
+++ src/components/inviteCode.tsx
@@ -21,14 +21,16 @@ const InviteCode = () => {
         <div className="px-4 md:px-0">
           <div className="p-4 md:px-0 w-full md:w-[1030px] min-h-[80px] mb-[71px] flex justify-center items-center border border-primary-default rounded-2xl">
             <div className="grid md:flex items-center gap-2 me-4">
-              <p className="text-xl font-normal">Get <b>50</b> <img className="w-[24px] inline-block" src="/credpoints/cred.svg" alt="swap" /> for every fren that joins Cred using your invite code:</p>
-              <p
-                className="flex justify-center items-center text-[26px] font-normal cursor-pointer hover:bg-[#ffffff1a] rounded-[50px] px-6 py-1"
-                onClick={handleCopyToClipboard}
-              >
-                {inviteCode}&nbsp;
-                <img className="inline-block w-[24px]" src="/copy.svg" alt="copy" />
-              </p>
+              <p className="text-base md:text-xl font-normal">Get <b>50</b> <img className="w-[24px] inline-block" src="/credpoints/cred.svg" alt="swap" /> for every fren that joins Cred using your invite code:</p>
+              <div className="justify-center flex">
+                <p
+                  className="w-[167px] flex justify-center items-center text-[22px] md:text-[26px] font-normal cursor-pointer hover:bg-[#ffffff1a] rounded-[50px] px-6 py-1"
+                  onClick={handleCopyToClipboard}
+                >
+                  {inviteCode}&nbsp;
+                  <img className="inline-block w-[24px]" src="/copy.svg" alt="copy" />
+                </p>
+              </div>
             </div>
           </div>
         </div>
