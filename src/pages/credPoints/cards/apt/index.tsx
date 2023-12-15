46230872644d4cea1073262a2b6a7e00a83842df
codewizard870 <alenzer0902@gmail.com>
2023-12-15 15:50:13 +0800

    issue 75 fixed

--- src/pages/credPoints/cards/apt/index.tsx
+++ src/pages/credPoints/cards/apt/index.tsx
@@ -17,7 +17,7 @@ const Apt = () => {
         </div>
       ) : (
         <div className="container w-[260px] md:w-[320px] min-h-[140px]  px-4 pt-4 flex flex-col items-center border border-gray-light-2 rounded-xl">
-          {aptTxsPercentage != undefined ? (
+          {aptTxsPercentage !== undefined && aptTxsPercentage !== 0 ? (
             <>
               <div className="flex items-center gap-2">
                 <img src="/credpoints/aptos.svg" alt="swap" />
@@ -35,7 +35,8 @@ const Apt = () => {
                 <p className="text-2xl font-bold">Favourite token</p>
               </div>
               <p className="mt-4 text-center text-xs md:text-sm">
-                empty state
+                Here we’ll show what’s the token you have interacted with the
+                most
               </p>
             </>
           )}
