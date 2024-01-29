d6c0997d045d1c3e992905cc30617923f42377c0
codewizard870 <alenzer0902@gmail.com>
2024-01-29 22:18:38 +0800

    fix/NFT images not showing

--- src/util/url.ts
+++ src/util/url.ts
@@ -2,6 +2,7 @@ export const getImageURL = (ipfsLink: string) => {
   if (ipfsLink.startsWith("ipfs://")) {
     const cid = ipfsLink.replace("ipfs://", "");
     return `https://ipfs.io/ipfs/${cid}`;
+  } else {
+    return ipfsLink;
   }
-  return ipfsLink;
 };
