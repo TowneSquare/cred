953005d628ddae745d2c878b08d2bfaede2d4e49
codewizard870 <alenzer0902@gmail.com>
2024-01-31 23:48:09 +0800

    add 3 defi types

--- src/constants/defi.ts
+++ src/constants/defi.ts
@@ -3,19 +3,28 @@ export const DefiIcon: { [key: string]: string } = {
   "pontem": "/credpoints/liquidSwap.svg",
   "pancake": "/credpoints/pancakeswap.png",
   "thala": "/credpoints/thala.svg",
-  "aries": "/credpoints/aries.png"
+  "aries": "/credpoints/aries.png",
+  "swap": "/credpoints/defi/swapgpt.svg",
+  "kana": "/credpoints/defi/kana_labs.svg",
+  "merkle": "/credpoints/defi/merkle_trade.svg"
 }
 
 export const DefiName: { [key: string]: string } = {
   "pontem": "Liquidswap",
   "pancake": "Pancakeswap",
   "thala": "Thala",
-  "aries": "Aries Market"
+  "aries": "Aries Market",
+  "swap": "SwapGPT",
+  "kana": "Kana Labs",
+  "merkle": "Merkle trade"
 }
 
 export const DefiLink: { [key: string]: string } = {
   "pontem": "https://pontem.network/liquidswap",
   "pancake": "https://pancakeswap.finance/",
   "thala": "https://app.thala.fi/",
-  "aries": "https://ariesmarkets.xyz/"
+  "aries": "https://ariesmarkets.xyz/",
+  "swap": "https://app.swapgpt.ai/trade/",
+  "kana": "https://tradebook.kanalabs.io/",
+  "merkle": "https://app.merkle.trade/"
 }
